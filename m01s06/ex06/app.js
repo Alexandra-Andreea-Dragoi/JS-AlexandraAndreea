a8c8551ab16ef362f774f24199ee4bef474e23ef
Alexandra Andreea Dragoi <69802787+Alexandra-Andreea-Dragoi@users.noreply.github.com>
2023-10-15 00:55:29 +0300

    JS08

--- m01s06/ex06/app.js
+++ m01s06/ex06/app.js
@@ -16,3 +16,11 @@ function paragraphLog(message) {
 console.domLog = paragraphLog;
 
 console.domLog('Nu apar in consola.');
+
+// Modifica exercitiul 06 astfel incat functia sa primeasca un numar nedeterminat de parametri, similar cu console.log(). Foloseste variabila arguments a functiei pentru a rula o bucla prin parametri.
+
+// for (let i = 0; i < arguments.length; i++) {
+//   message += arguments[i] + punctuation
+// }
+
+// Nu o vad pe asta, nu stiu sa o fac :((
